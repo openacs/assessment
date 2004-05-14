6bed627090e550390edeb9a9d49580678ac17543
Alvaro Rodriguez <alvaro@viaro.net>
2004-05-14 09:34:37 +0000

    Added assessment and section tables

--- sql/postgresql/assessment-drop.sql
+++ sql/postgresql/assessment-drop.sql
@@ -2,7 +2,15 @@ drop table as_item_choice_map;
 drop table as_item_choices;
 drop table as_item_attributes;
 drop table as_item_localized;
+
+drop table as_item_section_map;
+
 drop table as_items;
 drop table as_item_display_types;
 drop table as_item_type_attributes;
 drop table as_item_types;
+
+drop table as_assessment_section_map;
+drop table as_assessments;
+drop table as_sections;
+drop table as_section_display_types;
