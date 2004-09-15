43c6176ecde9fabb9251265935c14a0edc207f87
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2004-09-15 19:43:20 +0000

    fixes

--- www/unzip_file.tcl
+++ www/unzip_file.tcl
@@ -23,8 +23,8 @@ catch { exec unzip ${zipfile.tmpfile} -d $tmpdirectory } outMsg
 set qti_items_imported_number 0
 # Read the content of the temporary directory
 foreach file_i [ glob -directory $tmpdirectory *{.xml}  ] {
-	set numItems [parse_qti_xml $file_i]
-	set qti_items_imported_number [expr $numItems  + $qti_items_imported_number]	
+	set items [llength [parse_qti_xml $file_i]]
+	set qti_items_imported_number [expr [llength items]  + $qti_items_imported_number]	
 }
 
 # Delete the temporary directory
