68a966a6df7e51f89abb03440596f3f81953f6a8
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2004-09-21 17:02:58 +0000

    assessment admin files

---
+++ www/as_admin.tcl
@@ -0,0 +1,11 @@
+ad_page_contract {
+	@author Eduardo Pérez Ureta (eperez@it.uc3m.es)
+	@creation-date 2004-08-24
+} {
+} -properties {
+    context:onevalue
+}
+
+set context [list]
+
+ad_return_template
