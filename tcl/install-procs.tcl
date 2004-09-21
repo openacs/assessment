d154b526995b9d6f898428dbf672fcc012bb20ae
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2004-09-21 16:55:13 +0000

    fix package_instantiate

--- tcl/install-procs.tcl
+++ tcl/install-procs.tcl
@@ -118,12 +118,13 @@ content::type::register_relation_type -content_type {as_items} -target_type {as_
 }
 
 ad_proc -public assessment::install::package_instantiate {
-    
+    -package_id:required
+} {
     Define folders
     
 } {
 
-    set folder_id [content::folder::new -name {as_items} -package_id [ad_conn package_id] ]
+    set folder_id [content::folder::new -name {as_items} -package_id $package_id ]
     content::folder::register_content_type -folder_id $folder_id -content_type {as_item_choices} -include_subtypes t
     content::folder::register_content_type -folder_id $folder_id -content_type {as_item_type_mc} -include_subtypes t
     content::folder::register_content_type -folder_id $folder_id -content_type {as_item_display_rb} -include_subtypes t
