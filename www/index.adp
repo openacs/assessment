290dae41487f5a364ffdd31ec99774d5405f439c
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2004-09-23 18:48:59 +0000

    use list-builder for the assessments view

--- www/index.adp
+++ www/index.adp
@@ -3,12 +3,6 @@
 <property name="context">@context;noquote@</property>
 <if @admin_p@ eq "1"><p style="text-align: right;"><a href="admin/"><img src="graphics/admin.gif" border="0" alt="Administer Assessments"></a></if>
 
-<multiple name="assessment_info">
-	<ul>
-		<li>
-			<a href="assessment?assessment_id=@assessment_info.assessment_id@" title="@assessment_info.description@">@assessment_info.title@</a> <a href="sessions?assessment_id=@assessment_info.assessment_id@" title="@assessment_info.description@">sessions</a>
-		</li>
-	</ul>
-</multiple>
+<listtemplate name="assessments"></listtemplate>
 
 </master>
--- www/index.tcl
+++ www/index.tcl
@@ -11,7 +11,29 @@ set context [list "Show Assessments"]
 
 set package_id [ad_conn package_id]
 
-db_multirow assessment_info asssessment_id_name_definition {} {}
+template::list::create \
+    -name assessments \
+    -multirow assessments \
+    -key assessment_id \
+    -elements {
+        title {
+            label {Assessment}
+            link_url_eval {[export_vars -base "assessment" {assessment_id}]}
+            link_html { title {@assessments.description@} }
+
+        }
+        session {
+            label {Sessions}
+            link_url_eval {[export_vars -base "sessions" {assessment_id}]}
+        }
+    } \
+    -main_class {
+        narrow
+    }
+
+db_multirow -extend { session } assessments asssessment_id_name_definition {} {
+    set session {Sessions}
+}
 
 set admin_p [ad_permission_p $package_id admin]
 
