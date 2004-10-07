eb78c1afb2b695b31aa24351fc7506f6ef9fbc10
Timo Hentschel <timo@timohentschel.de>
2004-10-07 13:39:57 +0000

    made queries more readable

--- www/assessment.tcl
+++ www/assessment.tcl
@@ -11,8 +11,18 @@ ad_page_contract {
 set context [list "[_ assessment.Show_Items]"]
 
 set as_session_id [as_session_new -assessment_id $assessment_id -subject_id [ad_conn user_id]]
-set assessment_name [db_string assessment_name {SELECT as_assessmentsx.title FROM as_assessmentsx WHERE as_assessmentsx.assessment_id=:assessment_id}]
-db_dml session_start {UPDATE as_sessions SET creation_datetime = NOW() WHERE session_id=:as_session_id}
+
+set assessment_name [db_string assessment_name {
+    SELECT title
+    FROM as_assessmentsx
+    WHERE assessment_id = :assessment_id
+}]
+
+db_dml session_start {
+    UPDATE as_sessions
+    SET creation_datetime = NOW()
+    WHERE session_id = :as_session_id
+}
 
 ad_form -name show_item_form -action process-response -html {enctype multipart/form-data} -form {
     { as_session_id:text {value $as_session_id} }
