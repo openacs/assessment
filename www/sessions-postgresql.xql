a3faae3e3d800eb0fd2b5ed7a2f79b7ae02aa37a
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2004-12-20 14:01:46 +0000

    move some logic to last-session

--- www/sessions-postgresql.xql
+++ www/sessions-postgresql.xql
@@ -5,18 +5,7 @@
 
 	<fullquery name="sessions_of_assessment">
 		<querytext>
-			SELECT s.session_id, s.name, s.title, s.completed_datetime,
-			       p.first_names || ' ' || p.last_name AS subject_name,
-			       a.title AS assessment_name, s.subject_id
-			FROM as_sessionsx s INNER JOIN as_assessmentsx a USING (assessment_id)
-			INNER JOIN persons p ON (s.subject_id = p.person_id)
-			WHERE s.assessment_id = :assessment_id
-		</querytext>
-	</fullquery>
-
-	<fullquery name="sessions_of_assessment_of_subject">
-		<querytext>
-			SELECT s.session_id, s.name, s.title, s.completed_datetime,
+			SELECT s.session_id, s.name, s.title, s.completed_datetime, s.percent_score,
 			       p.first_names || ' ' || p.last_name AS subject_name,
 			       a.title AS assessment_name, s.subject_id
 			FROM as_sessionsx s INNER JOIN as_assessmentsx a USING (assessment_id)
@@ -24,6 +13,5 @@
 			WHERE s.assessment_id = :assessment_id
 			AND s.subject_id = :subject_id
 		</querytext>
-	</fullquery>
-	
+	</fullquery>	
 </queryset>
--- www/sessions.tcl
+++ www/sessions.tcl
@@ -8,6 +8,7 @@ ad_page_contract {
 	@creation-date 2004-09-03
 } {
   assessment_id:notnull
+  subject_id:notnull
 } -properties {
 	context:onevalue
 	assessment_info:multirow
@@ -18,7 +19,8 @@ set context [list "[_ assessment.Show_Sessions]"]
 set package_id [ad_conn package_id]
 
 #get the user that take an assessment
-set subject_id [ad_conn user_id]
+#set subject_id [ad_conn user_id]
+set subject_id $subject_id
 
 #Lists the identifier of sessions, the name of subjects that took this assessment, the name of assessment and the finished time 
 #of assessment for an assessment.
@@ -44,30 +46,24 @@ template::list::create \
 	    label {[_ assessment.Finish_Time]}
 	    html {nowrap}
 	}
+	percent_score {
+	    label {[_ assessment.Percent_Score]}
+	    html {nowrap}
+	}
     } \
     -main_class {
         narrow
     }
 
-set admin_p [ad_permission_p $package_id admin]
+
 
 set assessment_id_multi $assessment_id
 #if the user is admin he will display all sessions from all subjects
-if { $admin_p } {
 db_multirow -extend { item_url assessment_id } sessions sessions_of_assessment {
 } {
     set item_url [export_vars -base "session" {session_id}]
     set assessment_id $assessment_id_multi
 }
-#if the user is not admin he will display only his sessions
-} else {
-db_multirow -extend { item_url assessment_id } sessions sessions_of_assessment_of_subject {
-} {
-    set item_url [export_vars -base "session" {session_id}]
-    set assessment_id $assessment_id_multi
-}
-}
 
-set admin_p [ad_permission_p $package_id admin]
 
 ad_return_template
