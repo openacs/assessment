0e4d28d6e24e11a97aeb3daf87fe42d35990a6ef
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2005-01-13 18:24:09 +0000

    using just the revision_id as the file identifier instead of creating a relation

--- tcl/as-file-procs.tcl
+++ tcl/as-file-procs.tcl
@@ -27,7 +27,6 @@ ad_proc -public as::file::new {
     set file_mimetype [cr_filename_to_mime_type -create $filename]
     set content_length [file size $file_pathname]
     set content_rev_id [cr_import_content -title $filename $folder_id $file_pathname $content_length $file_mimetype [exec uuidgen]]
-    as::item_rels::new -item_rev_id $as_item_id -target_rev_id $content_rev_id -type as_item_content_rel
 
     return $content_rev_id
 }
