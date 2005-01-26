d842943f6ac66dee57b06cda4ad0dbf556e1b868
Anny Flores <annyflores@viaro.net>
2005-01-26 18:18:35 +0000

    validation for display of link

--- lib/section-items.tcl
+++ lib/section-items.tcl
@@ -2,12 +2,15 @@ ad_form -name admin_section -form {
     {section_id:text(hidden) {value $section_id}}
 }
 
-db_multirow -extend { presentation_type html } items section_items {} {
+db_multirow -extend { presentation_type html mc_type} items section_items {} {
     set presentation_type [as::item_form::add_item_to_form -name admin_section -section_id $section_id -item_id $as_item_id]
     if {$presentation_type == "fitb"} {
         regsub -all -line -nocase -- {<textbox as_item_choice_id=} $title "<input name=response_to_item.${as_item_id}_" html
     }
 
+    #used to determine if the item is multiple choice
+    
+    set mc_type [db_string item_type_id {} -default "nmc"]
     if {[empty_string_p $points]} {
 	set points 0
     }
