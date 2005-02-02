141a92fb5c958e1debe0f3b81e0d882cc8aed71e
Anny Flores <annyflores@viaro.net>
2005-02-02 19:04:46 +0000

    fix verification of permissions

--- www/asm-admin/index.adp
+++ www/asm-admin/index.adp
@@ -5,7 +5,8 @@
 <if @admin_p@ eq 1>
 <a
 href="@categories_url@cadmin/one-object?object_id=@package_id@">#assessment.admin_categories#</a>
-| <a href=../admin/asm-action-admin>#assessment.admin_actions#</a> | <a href=../admin/admin-request>#assessment.admin_requests#</a>
+| <a href=../admin/asm-action-admin>#assessment.admin_actions#</a> | <a href=../admin/admin-request>#assessment.admin_requests#</a> | <a href="permissions?object_id=@package_id@">#assessment.permissions#</a>
+
 </if>
 
 <p>
--- www/asm-admin/index.tcl
+++ www/asm-admin/index.tcl
@@ -19,6 +19,14 @@ set package_id [ad_conn package_id]
 set categories_url [db_string get_category_url {}]
 set user_id [ad_conn user_id]
 set admin_p [ad_permission_p $package_id admin]
+set package_admin [permission::permission_p -party_id $user_id -object_id $package_id -privilege "admin"]
+set m_name ""
+
+if { $package_admin == 0} {
+    set m_name "get_all_assessments_admin"
+} else {
+    set m_name "get_all_assessments"
+}
 
 #form to upload a QTI ZIP file
 ad_form -name form_upload_file -action {unzip-file} -html {enctype multipart/form-data}  -form {
@@ -32,8 +40,9 @@ if {[ad_permission_p [acs_magic_object "security_context_root"] "admin"]} {
 }
 
 #get all assessments order by title
-db_multirow -extend { export } assessments get_all_assessments {} {
+db_multirow -extend { export permissions} assessments $m_name {} {
     set export "[_ assessment.Export]"
+    set permissions "[_ assessment.permissions]"
 }
 
 #list all assessments
@@ -50,6 +59,11 @@ list::create \
 	    label "[_ assessment.Export]"
 	    link_url_eval "[export_vars -base export { assessment_id }]"
 	}
+	permissions {
+	    label "[_ assessment.permissions]"
+	    link_url_eval "[export_vars -base permissions { {object_id $assessment_id} }]"
+	}
+	
     } -actions $actions
 
 ad_return_template
