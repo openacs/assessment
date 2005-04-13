d66afa3332cc5745c38078f1b5952d14d1426718
Malte Sussdorff <openacs@sussdorff.de>
2005-04-13 11:11:05 +0000

    Made sure you can see a list of users who answered an anonymous assessment and when. It is not possible to see what they answered though.

--- www/asm-admin/results-users.tcl
+++ www/asm-admin/results-users.tcl
@@ -32,7 +32,7 @@ template::list::create \
     -elements {
 	session_id {
 	    label {[_ assessment.Session]}
-	    link_url_eval {[export_vars -base "results-session" {session_id}]}
+	    display_template {<if @results.result_url@ not nil><a href="@results.result_url@">@results.session_id@</a></if><else></else>}
 	}
 	subject_name {
 	    label {[_ assessment.Subject_Name]}
@@ -44,21 +44,25 @@ template::list::create \
 	}
 	percent_score {
 	    label {[_ assessment.Percent_Score]}
-	    link_url_eval {[export_vars -base "results-session" {session_id}]}
 	    html {align right nowrap}
+	    display_template {<if @results.result_url@ not nil><a href="@results.result_url@">@results.percent_score@</a></if><else></else>}
 	}
     } -main_class {
 	narrow
     } 
 
 
-db_multirow -extend { subject_url } results assessment_results {
+db_multirow -extend { result_url subject_url } results assessment_results {
 } {
     if {$assessment_data(anonymous_p) == "t" && $subject_id != $user_id} {
-	set subject_name "[_ assessment.anonymous_name]"
+
+	# You should be able to see who answered this assessment but not his answer.
+	# set subject_name "[_ assessment.anonymous_name]"
 	set subject_url ""
+	set result_url ""
     } else {
 	set subject_url [acs_community_member_url -user_id $subject_id]
+	set result_url [export_vars -base "results-session" {session_id}]
     }
 }
 
