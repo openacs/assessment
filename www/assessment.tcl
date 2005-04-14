8b688f455a1ab8da60c1556cfbb3520b7ece6bd8
Timo Hentschel <timo@timohentschel.de>
2005-04-14 14:52:58 +0000

    bugfix: calling wrong proc for as::item::item_data

--- www/assessment.tcl
+++ www/assessment.tcl
@@ -340,7 +340,7 @@ foreach one_item $item_list {
     set max_time_to_complete [as::assessment::pretty_time -seconds $max_time_to_complete]
 
     if {$presentation_type == "rb" || $presentation_type == "cb"} {
-	array set item [as::item_form::item_data -as_item_id $as_item_id]
+	array set item [as::item::item_data -as_item_id $as_item_id]
 	array set type [as::item_display_$presentation_type\::data -type_id $item(display_type_id)]
 	set choice_orientation $type(choice_orientation)
 	array unset item
