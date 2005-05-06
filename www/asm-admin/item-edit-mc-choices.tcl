9e5537776c55746f0ef35c9aedd9547acff1ee52
Stan Kaufman <skaufman@epimetrics.com>
2005-05-06 00:30:14 +0000

    partial fix of bug 2388

--- www/asm-admin/item-edit-mc-choices.tcl
+++ www/asm-admin/item-edit-mc-choices.tcl
@@ -15,7 +15,7 @@ ad_page_contract {
     percent:array,optional
     selected:array,optional
 } -properties {
-    context_bar:onevalue
+    context:onevalue
     page_title:onevalue
 }
 
@@ -33,7 +33,7 @@ if {![info exists assessment_data(assessment_id)]} {
 
 set package_id [ad_conn package_id]
 set page_title [_ assessment.edit_item_type_mc_choices]
-set context_bar [ad_context_bar [list [export_vars -base one-a {assessment_id}] $assessment_data(title)] [list [export_vars -base item-edit {assessment_id section_id as_item_id}] [_ assessment.edit_item]] $page_title]
+set context [list [list index [_ assessment.admin]] [list [export_vars -base one-a {assessment_id}] $assessment_data(title)] [list [export_vars -base item-edit {assessment_id section_id as_item_id}] [_ assessment.edit_item]] $page_title]
 
 set selected_options [list [list "[_ assessment.yes]" t]]
 
