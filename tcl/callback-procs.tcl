1d832c41c591dba8ef6afebece960e863fe78354
Dave Bauer <dave@thedesignexperience.org>
2005-08-22 02:49:54 +0000

    Add a default to parameter::get in case the paramter is not set.

--- tcl/callback-procs.tcl
+++ tcl/callback-procs.tcl
@@ -7,7 +7,7 @@ ad_proc -public -callback user::registration -impl asm_url {} {
     Return the properly formed link (URL) that the user will click to go into the registration process.
     
 } {
-    set assessment_id [parameter::get -parameter RegistrationId]
+    set assessment_id [parameter::get -parameter RegistrationId -default 0]
     if { $assessment_id != 0 } {
 	
 	set package_id [db_string package_id {}]
