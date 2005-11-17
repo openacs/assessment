95af60ea2cad2000144d41b46f6ee6f4a9cfef03
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2005-11-17 15:41:46 +0000

    imsld::import infrastructure

--- tcl/as-callback-procs.tcl
+++ tcl/as-callback-procs.tcl
@@ -15,6 +15,17 @@ ad_proc -public -callback lors::import -impl qti {} {
 	}
 }
 
+ad_proc -public -callback imsld::import -impl qti {} {
+    this is the imsld qti importer
+} {
+	if {$res_type == "imsqti_xmlv1p0" || $res_type == "imsqti_xmlv1p1" || $res_type =="imsqti_item_xmlv2p0"} {
+	    return [as::qti::register_xml_object_id \
+			-xml_file $tmp_dir/$res_href \
+			-community_id $community_id]
+	}
+}
+
+
 ad_proc -callback merge::MergeShowUserInfo -impl as {
     -user_id:required
 } {
