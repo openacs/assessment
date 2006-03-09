93c6916499ba10ead161485ec865cbe59ddc9365
Dave Bauer <dave@thedesignexperience.org>
2006-03-09 22:38:26 +0000

    Fix another double substitution bug.

--- tcl/as-item-display-rb-procs.tcl
+++ tcl/as-item-display-rb-procs.tcl
@@ -132,7 +132,7 @@ ad_proc -public as::item_display_rb::render {
     if {$required_p != "t"} {
 	set optional ",optional"
     }
-    set param_list [list [list label $title] [list help_text $subtext] [list value $default_value] [list options $data] [list html $type(html_display_options)]]
+    set param_list [list [list label \$title] [list help_text \$subtext] [list value \$default_value] [list options \$data] [list html \$type(html_display_options)]]
     set element_params [concat [list "$element\:text(radio)$optional"] $param_list]
 
     ad_form -extend -name $form -form [list $element_params]
