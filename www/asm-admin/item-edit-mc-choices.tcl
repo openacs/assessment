31c86626b8db1574ecd3977dcda73c0e06c07336
Dave Bauer <dave@thedesignexperience.org>
2008-08-29 19:13:34 +0000

    Use context instead of context_bar

--- www/asm-admin/item-edit-mc-choices.tcl
+++ www/asm-admin/item-edit-mc-choices.tcl
@@ -33,7 +33,7 @@ if {![info exists assessment_data(assessment_id)]} {
 
 set package_id [ad_conn package_id]
 set page_title [_ assessment.edit_item_type_mc_choices]
-set context_bar [ad_context_bar [list [export_vars -base one-a {assessment_id}] $assessment_data(title)] [list [export_vars -base item-edit {assessment_id section_id as_item_id}] [_ assessment.edit_item]] $page_title]
+set context [list [list [export_vars -base one-a {assessment_id}] $assessment_data(title)] [list [export_vars -base questions {assessment_id}] Questions] [list [export_vars -base item-edit {assessment_id section_id as_item_id}] [_ assessment.edit_item]] $page_title]
 
 set selected_options [list [list "[_ assessment.yes]" t]]
 
