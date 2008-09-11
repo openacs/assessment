5d67920e2f3d1f88ed47b498126af782f2d3c525
Raúl Morales Hidalgo <rmorales@innova.uned.es>
2008-09-11 08:16:30 +0000

    wrong edit this question url

--- lib/session-items.tcl
+++ lib/session-items.tcl
@@ -39,7 +39,7 @@ ad_form -name Xsession_results_$section_id -mode display -form {
 set feedback_count 0
 db_multirow -extend { presentation_type html result_points feedback answered_p choice_orientation next_as_item_id next_pr_type num content has_feedback_p correct_p view item_edit_general_url results_edit_url} items session_items {} {
     # build URLs
-    set item_edit_general_url [export_vars -base "asm-admin/item-edit-general" {as_item_id assessment_id section_id}]
+    set item_edit_general_url [export_vars -base "item-edit-general" {as_item_id assessment_id section_id}]
     set results_edit_url [export_vars -base "results-edit" {session_id section_id as_item_id}]
 
     set default_value [as::item_data::get -subject_id $subject_id -as_item_id $as_item_id -session_id $session_id]
