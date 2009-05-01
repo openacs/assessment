a17150faf5bb1cd1fd3c18336da165279bbbe200
Alvaro Rodriguez <alvaro@viaro.net>
2009-05-01 18:56:29 +0000

    Change the order of the line added in the previous commit for a new question type, new question types should be added at the end to avoid changing the order of the current question types because in item-add.adp the types are displayed using the indexes

--- tcl/as-item-type-procs.tcl
+++ tcl/as-item-type-procs.tcl
@@ -28,8 +28,8 @@ ad_proc -public as_item_type::get_item_types {
     foreach item_type [db_list item_types {}] {
         lappend item_types [list "[_ assessment.item_type_$item_type]" $item_type]
     }
-    lappend item_types [list "[_ assessment.Single_Response_Allowed_Dropdown_Box]" sb]
     lappend item_types [list "[_ assessment.item_type_ms]" ms]
+    lappend item_types [list "[_ assessment.Single_Response_Allowed_Dropdown_Box]" sb]
     return $item_types
 }
 
