be6e41302c6d85409b8337ca033b018bdb05b198
gernst <>
2017-09-22 13:23:06 +0000

    Fixed wrong call of db_multirow

--- lib/results-messages.tcl
+++ lib/results-messages.tcl
@@ -3,8 +3,7 @@
 
 set format "[lc_get d_fmt], [lc_get t_fmt]"
 
-db_multirow -unclobber -extend {session_url user_url} results result_changes {
-} {
+db_multirow -unclobber -extend {session_url user_url} results result_changes {} {
     set creation_date [lc_time_fmt $creation_date $format]
     set user_url [acs_community_member_url -user_id $creation_user]
     set session_url [export_vars -base results-session {session_id}]
