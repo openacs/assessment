d862f23b2a97f1933580297d7c8be0d01c4fc045
Antonio Pisano <antonio@elettrotecnica.it>
2018-05-29 17:21:00 +0000

    Beautify code, remove unneded variables and queries

--- tcl/as-apm-checks-procs.tcl
+++ tcl/as-apm-checks-procs.tcl
@@ -9,40 +9,37 @@ ad_library {
 namespace eval inter_item_checks::notification_delivery {
 
     ad_proc -public do_notification {
-     -inter_item_check_id
-     -user_id
-     -interval_id
+        -inter_item_check_id
+        -user_id
+        -interval_id
     } {
-    } { 
-
-
-
-	db_1row select_inter_item_check_name {}
-        db_1row select_user_name {}
-	set text ""
-
-        set package_id [ad_conn package_id]
-
-    # Notifies the users that requested notification for the specific INTER_ITEM_CHECK
+    } {        
+	db_1row select_inter_item_check_name {
+            select inter_item_check_name
+            from inter_item_checks
+            where inter_item_check_id = :inter_item_check_id
+        }
+        
+        # Notifies the users that requested notification for the specific INTER_ITEM_CHECK
 
 	set method_id [notification::get_delivery_method_id -name "email"]
-     
+        set type_id [notification::type::get_type_id \
+                         -short_name inter_item_check_notif]
+        
 	notification::new \
-	    -type_id [notification::type::get_type_id  -short_name inter_item_check_notif] \
+	    -type_id $type_id \
 	    -object_id $inter_item_check_id \
 	    -notif_subject "Testing notification"\
-	    -notif_text "I works"
+	    -notif_text "It works"
 	
 	notification::request::new \
-	    -type_id [notification::type::get_type_id \
-			  -short_name inter_item_check_notif] \
+	    -type_id $type_id \
 	    -object_id $inter_item_check_id \
 	    -user_id $user_id \
 	    -interval_id $interval_id \
-	    -delivery_method_id $method_id
-	
-	
+	    -delivery_method_id $method_id		
     }
+    
 }
 
 # Local variables:
