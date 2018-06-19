923cba3fc842e499bc2ab10d71517b6ad06a7295
Antonio Pisano <antonio@elettrotecnica.it>
2018-06-19 07:48:37 +0000

    Reduce divergency between oracle and postgres codebase

--- lib/comments-chunk.tcl
+++ lib/comments-chunk.tcl
@@ -29,7 +29,24 @@ set general_comments_url [apm_package_url_from_key "general-comments"]
 if {[set has_permission_p [permission::permission_p -object_id $object_id -privilege "general_comments_create"]]} {
     set comment_add_url [export_vars -base "${general_comments_url}comment-add" {object_id return_url}]
 
-    db_multirow -extend { html_content edit_url } comments comments {} {
+    db_multirow -extend { html_content edit_url author } comments comments {
+	  select g.comment_id,
+           r.content,
+           r.title,
+           r.mime_type,
+           o.creation_user,
+           to_char(o.creation_date, 'YYYY-MM-DD HH24:MI:SS') as creation_date_ansi
+	  from general_comments g,
+         cr_revisions r,
+         cr_items ci,
+         acs_objects o
+	  where g.object_id = :object_id
+          and r.revision_id = ci.live_revision
+          and ci.item_id = g.comment_id
+          and o.object_id = g.comment_id
+	  order by o.creation_date
+    } {
+        set author [acs_user::get_element -user_id $creation_user -element "name"]
         set html_content [ad_html_text_convert -from $mime_type -- $content]
         set edit_url [export_vars -base "${general_comments_url}comment-edit" {comment_id return_url}]
     }
