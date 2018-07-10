42a05baac35edd44b35d0a1ff17bdc01b4975385
Antonio Pisano <antonio@elettrotecnica.it>
2018-07-10 15:21:11 +0000

    Add documentation for public procs (fixes automated tests), use an api that will assume a single mountpoint for the package (as was intended)

--- tcl/as-notification-procs.tcl
+++ tcl/as-notification-procs.tcl
@@ -9,15 +9,18 @@ namespace eval as::notification {}
 ad_proc -public as::notification::get_url {
     object_id
 } {
+    NotificationType.GetURL Service Contract implementation.
+} {
     set package_id [db_string get_package_id {}]
-    set package_url [site_node::get_url_from_object_id -object_id $package_id]
+    set package_url [apm_package_url_from_id $package_id]
     return "${package_url}asm-admin/one-a?assessment_id=$object_id"
 }
 
 ad_proc -public as::notification::process_reply {
     reply_id
 } {
-}
+    NotificationType.ProcessReply Service Contract implementation.
+} -
 
 # Local variables:
 #    mode: tcl
