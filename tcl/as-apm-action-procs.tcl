e5022c1b9b94368b44cadb0f1f5d4f817ee58880
Antonio Pisano <antonio@elettrotecnica.it>
2022-09-13 11:58:42 +0000

    Make package callbacks private, formatting changes

--- tcl/as-apm-action-procs.tcl
+++ tcl/as-apm-action-procs.tcl
@@ -1,4 +1,7 @@
 ad_library {
+
+    Action callbacks
+
     @author Anny Flores (annyflores@viaro.net) Viaro Networks (www.viaro.net)
     @creation-date 2004-12-03
 
@@ -18,29 +21,21 @@ ad_proc -private as::actions::get_admin_user_id {} {
     }]
 }
 
-ad_proc -public as::actions::insert_actions {
+ad_proc -private as::actions::insert_actions {
     {-package_id}
     {-node_id}
 } {
-} {
-
-# The code does not work at all on Oracle
+    # The code does not work at all on Oracle
     set user_id [as::actions::get_admin_user_id]
 
     db_exec_plsql insert_default {}
 }
 
-ad_proc -public as::actions::insert_actions_after_upgrade {
-
-} {
-} {
+ad_proc -private as::actions::insert_actions_after_upgrade {} {
     db_exec_plsql after_upgrade {}
-
 }
-ad_proc -public as::actions::update_checks_after_upgrade {
 
-} {
-} {
+ad_proc -private as::actions::update_checks_after_upgrade {} {
     set checks [db_list_of_lists get_all_checks {}]
 
     foreach check $checks {
@@ -53,7 +48,6 @@ ad_proc -public as::actions::update_checks_after_upgrade {
         append check_sql $append_sql
 
         db_dml update_checks {}
-
     }
 }
 
